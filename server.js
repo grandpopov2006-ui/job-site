d555da11d140b530b342dbb46708b6363729efc2
VeTaL <grandpopov2006@gmail.com>
2026-05-28 00:57:11 +0300

    remove backend route

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend работает");
-});
+
 
 app.post("/api/applications", async (req, res) => {
   console.log("Заявка пришла:", req.body);
